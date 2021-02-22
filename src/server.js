e0eaf5f18bba7a89e3904de6d3ccf0cb46eb1774
placiderapson <placideirandora@gmail.com>
2021-02-22 15:45:50 +0200

    Setup EJS as the View/Template Engine

--- src/server.js
+++ src/server.js
@@ -1,9 +1,17 @@
+import path from 'path';
 import express from 'express';
 import { config } from 'dotenv';
 
 config();
 
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  return res.render('index');
+});
 
 const port = process.env.PORT || 3000;
 
