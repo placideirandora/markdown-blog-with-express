8774e184926ec1d9248eafc7d0e313971bc64915
placiderapson <placideirandora@gmail.com>
2021-02-22 15:26:11 +0200

    Setup a Minimal Server with Express

---
+++ src/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { config } from 'dotenv';
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`App Listening on Port: ${port}`);
+});
