b7be88b9b7223d17d396f0d125f534b5123f5ea6
placiderapson <placideirandora@gmail.com>
2021-02-23 09:08:20 +0200

    Create Routes and Controllers for Rendering Publish Article page, View Single Article page, and Store New Article

--- src/routes/article.js
+++ src/routes/article.js
@@ -1,9 +1,41 @@
 import { Router } from 'express';
 
+import Article from '../models/Article';
+
 const articleRouter = Router();
 
 articleRouter.get('/new', (req, res) => {
-  return res.render('article/new');
+  const article = new Article();
+
+  return res.render('article/new', { article });
+});
+
+articleRouter.get('/:id', async (req, res) => {
+  const article = await Article.findOne({ _id: req.params.id });
+
+  if (!article) return res.redirect('/');
+
+  return res.render('article/show', { article });
+});
+
+articleRouter.post('/', async (req, res) => {
+  const article = new Article({
+    title: req.body.title,
+    image: req.body.image,
+    body: req.body.body,
+    markdown: req.body.markdown,
+    source: req.body.source
+  });
+
+  try {
+    const storedArticle = await article.save();
+
+    return res.redirect(`/article/${storedArticle._id}`);
+  } catch (error) {
+    console.log(error);
+
+    return res.render('article/new', { article });
+  }
 });
 
 export default articleRouter;
